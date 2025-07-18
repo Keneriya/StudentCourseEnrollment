e66984f2403867537b2e4c15077d97084099ef14
Keneriya <kenuts217@gmail.com>
2025-07-18 23:35:26 +0300

    init commit

---
+++ Student-Course-Enrollment-System/src/main/java/com/example/Student/Course/Enrollment/System/Entity/Enrollment.java
@@ -0,0 +1,25 @@
+package com.example.Student.Course.Enrollment.System.Entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "enrollments")
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate enrollmentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    // Getters and setters
+}

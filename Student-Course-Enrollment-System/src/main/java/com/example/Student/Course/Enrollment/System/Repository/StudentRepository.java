fc667c3c74c2d4d7fd28b1fa935b806f744b5757
Keneriya <kenuts217@gmail.com>
2025-07-20 12:54:11 +0300

    commit

--- Student-Course-Enrollment-System/src/main/java/com/example/Student/Course/Enrollment/System/Repository/StudentRepository.java
+++ Student-Course-Enrollment-System/src/main/java/com/example/Student/Course/Enrollment/System/Repository/StudentRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByEmail(String email);
-    List<Student> findByIsDeletedFalse();
+    List<Student> findByIsDeletedFalse();//so this method filters out logically deleted students.
     List<Student> findByRegistrationDateAfter(LocalDate date);
     List<Student> findByFullNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String name, String email);
 }

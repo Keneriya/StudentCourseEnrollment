d17977d5d0e09cdec51f433ea19f3d9ca23530db
Keneriya <kenuts217@gmail.com>
2025-07-20 12:46:39 +0300

    commit

---
+++ Student-Course-Enrollment-System/src/main/java/com/example/Student/Course/Enrollment/System/Repository/StudentRepository.java
@@ -0,0 +1,17 @@
+package com.example.Student.Course.Enrollment.System.Repository;
+
+import com.example.Student.Course.Enrollment.System.Entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+    List<Student> findByIsDeletedFalse();
+    List<Student> findByRegistrationDateAfter(LocalDate date);
+    List<Student> findByFullNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String name, String email);
+}
